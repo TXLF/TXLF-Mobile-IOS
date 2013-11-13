89a69a17360fd02ea3bd4b31cd870218d461b8ab
George Nixon <george@big.odlenixon.com>
2013-11-13 01:52:39 -0600

    Added code/classes for scanner to project from https://github.com/joelind/zxing-iphone/

---
+++ TXLF/Classes/OverlayView.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2009 Jeff Verkoeyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+
+@protocol CancelDelegate;
+
+@interface OverlayView : UIView {
+	UIImageView *imageView;
+	NSArray *_points;
+	UIButton *cancelButton;
+	id<CancelDelegate> delegate;
+	BOOL oneDMode;
+}
+
+@property (nonatomic, retain) NSArray*  points;
+@property (nonatomic, assign) id<CancelDelegate> delegate;
+@property (nonatomic, assign) BOOL oneDMode;
+
+
+- (id)initWithFrame:(CGRect)theFrame cancelEnabled:(BOOL)isCancelEnabled oneDMode:(BOOL)isOneDModeEnabled;
+
+- (CGRect)cropRect;
+
+@end
+
+@protocol CancelDelegate
+- (void)cancelled;
+@end
